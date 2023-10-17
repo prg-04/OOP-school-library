23094af1e98490dc0c76ea5faa05bf3f7e4e08f0
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-17 15:25:56 +0100

    refactor completed

---
+++ teacher_creator.rb
@@ -0,0 +1,16 @@
+class TeacherCreator
+    def initialize(people)
+    @people = people
+  end
+  def create_teacher
+    print "Enter the teacher's age: "
+    age = gets.chomp.to_i
+    print "Enter the teacher's name: "
+    name = gets.chomp
+    print "Enter the teacher's specialization: "
+    specialization = gets.chomp
+    teacher = Teacher.new(age: age, specialization: specialization, name: name)
+    @people << teacher
+    puts "Teacher created successfully! (ID: #{teacher.id})"
+  end
+end
